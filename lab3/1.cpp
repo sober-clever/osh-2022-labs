864fcc4324284a2e5e617da97e4ee7d5d014d262
Sober_Clever <Sober_Clever@mail.ustc.edu.cn>
2022-05-30 21:21:17 +0800

    Add big message transmission

--- lab3/1.cpp
+++ lab3/1.cpp
@@ -48,31 +48,44 @@ void *handle_chat(void *data) {
 }
 
 int main(int argc, char **argv) {
+     // 套接字所在的端口号由运行时的命令行参数给定
     int port = atoi(argv[1]);
     int fd;
+    // 创建套接字
     if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket");
         return 1;
     }
+    printf("Server socket %d is created.\n", fd);
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(port);
     socklen_t addr_len = sizeof(addr);
+    
+    // 将该套接字与地址绑定，把地址族中的地址赋给套接字
     if (bind(fd, (struct sockaddr *)&addr, sizeof(addr))) {
         perror("bind");
         return 1;
     }
+
+    // 监听套接字，同时将套接字变成被动类型，等待客户机连接
     if (listen(fd, 2)) {
         perror("listen");
         return 1;
     }
+
+    // 创建两个连接
     int fd1 = accept(fd, NULL, NULL);
+    //printf("Socket %d coonected.\n", fd1);
     int fd2 = accept(fd, NULL, NULL);
+    //printf("Scoket %d connected.\n", fd2);
     if (fd1 == -1 || fd2 == -1) {
         perror("accept");
         return 1;
     }
+
+    // 创建两个线程
     pthread_t thread1, thread2;
     struct Pipe pipe1;
     struct Pipe pipe2;
@@ -82,7 +95,12 @@ int main(int argc, char **argv) {
     pipe2.fd_recv = fd1;
     pthread_create(&thread1, NULL, handle_chat, (void *)&pipe1);
     pthread_create(&thread2, NULL, handle_chat, (void *)&pipe2);
+    //printf("Thread created.\n");
+    // 等待两个线程运行结束
     pthread_join(thread1, NULL);
+    //printf("Thread 1 exited.\n");
     pthread_join(thread2, NULL);
+    //printf("Thread 2 exited.\n");
     return 0;
 }
+
